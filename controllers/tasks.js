7e136bffd0935afa29dc7cb412e5a9709accde61
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-05-12 22:23:33 -0400

    setup get all tasks and get single task controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,36 @@
 const Task = require('../models/Task');
 
-const getAllTasks = (req, res) => {
-  res.send('all items');
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  const { id: taskID } = req.params;
+  try {
+    const singleTask = await Task.findById(taskID);
+    if (!singleTask) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ singleTask });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+
+  // res.json({ id: req.params.id });
 };
 
 const updateTask = (req, res) => {
